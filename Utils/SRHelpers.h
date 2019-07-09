ec18a71d28cff3d953a06e0ad1e5a4e1977b7c76
johannesmenzel <johannes.menzel@posteo.de>
2019-07-09 23:11:59 +0200

    Update SRHelpers.h
    Add voxengos fast tanh algo

--- Utils/SRHelpers.h
+++ Utils/SRHelpers.h
@@ -38,6 +38,17 @@ namespace SR {
       return (x - 1) / (x + 1);
     }
 
+    inline double vox_fasttanh2(const double x)
+    {
+      const double ax = fabs(x);
+      const double x2 = x * x;
+
+      return(x * (2.45550750702956 + 2.45550750702956 * ax +
+        (0.893229853513558 + 0.821226666969744 * ax) * x2) /
+        (2.44506634652299 + (2.44506634652299 + x2) *
+          fabs(x + 0.814642734961073 * x * ax)));
+    }
+
   }
 }
 // end namespaces
